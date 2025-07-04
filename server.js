384c8c2f89e5c7c434a172b99c55ddf2c816fddb
noneclashofclans <rishitmohanty3@gmail.com>
2025-07-04 19:32:30 +0530

    rechanged

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ✅ Use memory storage (no saving to disk)
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Upload route
